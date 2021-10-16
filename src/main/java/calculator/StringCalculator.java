16e03d5e65ecbaae3bad858dfd4ce7ff75ff8c89
Oz1one <48507452+Oz1one@users.noreply.github.com>
2021-10-16 14:31:16 +0530

    created separate delimiter method for cleaner code

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,15 +5,19 @@ class StringCalculator {
     public int add(String input) {
         if(input==null || input.equals("")) //check if input string is empty
     	return 0;
-        
+       
         else {
-        	String []newarr=input.split(",|\n"); //created new array that stores numbers seperated by ","
+        	
+        	 String [] newarr1=delimiter(input);//created new array that stores numbers seperated by ","
         	int sum=0;
-        	for(String s: newarr) {
+        	for(String s: newarr1) {
         		sum+=Integer.parseInt(s); //parsing the element of array as integers and adding them.
         	}
         	return sum;
         }
     }
-
+    private static String[] delimiter(String input1) {
+    	String []newarr1=input1.split(",|\n");
+    	return newarr1;
+    }
 }
