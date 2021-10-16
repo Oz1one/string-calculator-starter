0698f5f52799e2680b2b69478409358f00c45c76
Oz1one <48507452+Oz1one@users.noreply.github.com>
2021-10-16 13:55:37 +0530

    modified add method to allow both comma and newline(\n) as delimiters and wrote tests for the same

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,14 +3,14 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        if(input==null || input.equals(""))
+        if(input==null || input.equals("")) //check if input string is empty
     	return 0;
         
         else {
-        	String []newarr=input.split(",");
+        	String []newarr=input.split(",|\n"); //created new array that stores numbers seperated by ","
         	int sum=0;
         	for(String s: newarr) {
-        		sum+=Integer.parseInt(s);
+        		sum+=Integer.parseInt(s); //parsing the element of array as integers and adding them.
         	}
         	return sum;
         }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -19,9 +19,29 @@ class StringCalculatorShould {
         
         assertEquals(1, stringCalculator.add("1"));
     }
+    //testing if method works for two 1-digit numbers
     @Test
     void string_with_two_numbers_should_return_sum() {
     	assertEquals(9, stringCalculator.add("2,7"));
     }
-    
+    //testing if method works for two 2-digit numbers
+    @Test
+    void string_with_two_digit_numbers_should_return_sum() {
+    	assertEquals(99,stringCalculator.add("22,77"));
+    }
+    //testing if method works for unknown amount of digits
+    @Test
+    void string_with_any_amount_of_digits() {
+    	assertEquals(204,stringCalculator.add("3,4,8,84,105"));
+    }
+    //testing if method supports \n as new delimiter
+    @Test
+    void string_with_new_line_as_delimiter() {
+    	assertEquals(90, stringCalculator.add("20\n70"));
+    }
+    //testing if method supports both \n and , as delimiter
+    @Test
+    void string_with_new_line_and_comma_as_delimiter() {
+    	assertEquals(90, stringCalculator.add("20,20\n50"));
+    }
 }
